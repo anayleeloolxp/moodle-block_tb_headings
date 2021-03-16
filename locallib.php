c236e14b0155986392055e88a81a8de7e8df6f7b
Anay <anay.infowind@gmail.com>
2021-03-16 17:49:43 +0530

    Removed data if wrong license

--- locallib.php
+++ locallib.php
@@ -52,6 +52,7 @@ function updateconfheadings() {
     if ($infoleeloolxp->status != 'false') {
         $leeloolxpurl = $infoleeloolxp->data->install_url;
     } else {
+        set_config('settingsjson', base64_encode($output), 'block_tb_headings');
         return;
     }
     $url = $leeloolxpurl . '/admin/Theme_setup/get_heading_settings';
